09386986fda67236cd00190c371ca4286172ce47
dgusfr <diegogustavo.fr@gmail.com>
2024-11-15 17:02:50 -0300

    ADD DATA BASE

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 const express = require("express");
-const app = express();
-const bodyParser = required("body-parser");
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
 
+const app = express();
+
+// Configuração do template engine
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extends: flase }));
+// Configuração do body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Configuração de arquivos estáticos
 app.use(express.static("public"));
 
+// Conexão com o banco de dados
+connection
+  .authenticate()
+  .then(() => {
+    console.log("Conexão feita com sucesso");
+  })
+  .catch((error) => {
+    console.log("Erro ao conectar com o banco de dados:", error);
+  });
+
+// Rotas
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+// Inicialização do servidor
 app.listen(3000, () => {
-  console.log("O servidor está rodando!");
+  console.log("O servidor está rodando na porta 3000!");
 });
