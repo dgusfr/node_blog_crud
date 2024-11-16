91a308a9b59758a3a47aa2e2da5af5d7597e429b
dgusfr <diegogustavo.fr@gmail.com>
2024-11-16 10:06:14 -0300

    feat(add): models categories

---
+++ models/Category.js
@@ -0,0 +1,14 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/database");
+const Category = connection.define("categories", {
+  title: {
+    type: Sequelize.STRING,
+    ALLOWnULL: false,
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+modules.exports = Category;
