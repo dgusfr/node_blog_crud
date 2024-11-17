c33d6b85a4ca03b4beb2ee611d678b4b42bc9203
dgusfr <diegogustavo.fr@gmail.com>
2024-11-16 21:15:48 -0300

    styles(refact)

--- controllers/CategoriesController.js
+++ controllers/CategoriesController.js
@@ -1,12 +1,4 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/categories", (req, res) => {
-  res.send("ROTA DE CATEGORIAS");
-});
-
-router.get("/admin/categories/new", (req, res) => {
-  res.send("Rota para criar nova categoria!");
-});
-
 module.exports = router;
