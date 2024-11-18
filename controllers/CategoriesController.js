b7383738d540c2fc02e497aeb6155db6b6f0e74b
dgusfr <diegogustavo.fr@gmail.com>
2024-11-18 09:38:17 -0300

    feat(index)

--- controllers/CategoriesController.js
+++ controllers/CategoriesController.js
@@ -25,7 +25,7 @@ router.post("/categories/save", (req, res) => {
 });
 
 router.get("/admin/categories", (req, res) => {
-  res.render("admin/categories/new");
+  res.render("admin/categories/index");
 });
 
 module.exports = router;
