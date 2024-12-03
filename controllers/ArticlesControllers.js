de8aa7e3e23d2df08bd3e079c2d5dfed83dc64ed
dgusfr <diegogustavo.fr@gmail.com>
2024-12-03 10:17:05 -0300

    feat(add): new articles

--- controllers/ArticlesControllers.js
+++ controllers/ArticlesControllers.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../models/Category");
 
-router.get("/articles", (req, res) => {
-  res.send("Rota de artigos");
-});
-
+// Rota para exibir o formulário de criação de um novo artigo
 router.get("/admin/articles/new", (req, res) => {
-  res.send("Rota para criar novo artigo!");
+  Category.findAll()
+    .then((categories) => {
+      res.render("admin/articles/new", { categories: categories });
+    })
+    .catch((error) => {
+      console.error("Erro ao carregar categorias:", error);
+      res.redirect("/admin/articles");
+    });
 });
 
 module.exports = router;
