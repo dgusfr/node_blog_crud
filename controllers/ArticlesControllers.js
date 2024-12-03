261225dc7644754018ddf3ab9a1c6ddd49958974
dgusfr <diegogustavo.fr@gmail.com>
2024-12-03 10:48:39 -0300

    FEAT(ADD): ARTICLES

--- controllers/ArticlesControllers.js
+++ controllers/ArticlesControllers.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../models/Category");
+const Article = require("../models/Article");
+const slugify = require("slugify");
 
 // Rota para exibir o formulário de criação de um novo artigo
 router.get("/admin/articles/new", (req, res) => {
@@ -11,6 +13,33 @@ router.get("/admin/articles/new", (req, res) => {
     .catch((error) => {
       console.error("Erro ao carregar categorias:", error);
       res.redirect("/admin/articles");
+    });
+});
+
+// Rota para salvar o artigo
+router.post("/articles/save", (req, res) => {
+  const title = req.body.title;
+  const body = req.body.body;
+  const category = req.body.category;
+
+  // Verifica se os campos obrigatórios foram preenchidos
+  if (!title || !body || !category) {
+    console.error("Erro: Campos obrigatórios não preenchidos.");
+    return res.redirect("/admin/articles/new");
+  }
+
+  Article.create({
+    title: title,
+    slug: slugify(title),
+    body: body,
+    categoryId: category,
+  })
+    .then(() => {
+      res.redirect("/admin/articles");
+    })
+    .catch((error) => {
+      console.error("Erro ao salvar o artigo:", error);
+      res.redirect("/admin/articles/new");
     });
 });
 
