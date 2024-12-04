b4b6ec12341b1979d0767941b5c4b7dd3148e862
dgusfr <diegogustavo.fr@gmail.com>
2024-12-04 09:44:52 -0300

    feat(add: create user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("./User");
+const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 
 // Página de usuários
