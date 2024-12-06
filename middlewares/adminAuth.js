097a9499cd97876f67aba22b8057e3e7334f0c54
dgusfr <diegogustavo.fr@gmail.com>
2024-12-06 13:35:08 -0300

    fix(update): admin auth

--- middlewares/adminAuth.js
+++ middlewares/adminAuth.js
@@ -2,6 +2,7 @@ function adminAuth(req, res, next) {
   if (req.session.user && req.session.user.id) {
     next();
   } else {
+    console.log("Acesso negado: Usuário não autenticado.");
     res.redirect("/login?error=auth");
   }
 }
