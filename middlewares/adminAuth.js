ac59192553648dd5a4742303a1197736e841b65d
dgusfr <diegogustavo.fr@gmail.com>
2024-12-06 10:43:42 -0300

    feat(add): middleware auth

--- middlewares/adminAuth.js
+++ middlewares/adminAuth.js
@@ -1,9 +1,9 @@
-function adminAuth(req, res, next){
-   if(req.session.user != undefined){
-       next();
-   }else{
-       res.redirect("/login");
-   }
+function adminAuth(req, res, next) {
+  if (req.session.user && req.session.user.id) {
+    next();
+  } else {
+    res.redirect("/login?error=auth");
+  }
 }
 
-module.exports = adminAuth
+module.exports = adminAuth;
